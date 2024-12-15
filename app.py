730cfe4bf856b4c8621d2bf97cc6b9a534a36842
b-hundreds <hasybach1103@gmail.com>
2024-12-15 14:06:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
+# from flask import Flask, render_template, request, jsonify
+# from pymongo import MongoClient
+
+# app = Flask(__name__)
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['nasa_english']
+# collection = db['users']
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         name = data.get('name')
+#         email = data.get('email')
+#         phone = data.get('phone')
+
+#         collection.insert_one({"name": name, "email": email, "phone": phone})
+#         return jsonify({"message": "Data saved successfully!"}), 200
+
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
-db = client['nasa_english']
-collection = db['users']
+
+# Cấu hình kết nối với Google Sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("elegant-racer-444806-d0-f89220529ff5.json", scope)
+client = gspread.authorize(creds)
+
+# Mở Google Sheet
+sheet = client.open("nasa_info").sheet1  # Tên Google Sheet và sheet1 là trang đầu tiên
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,8 +43,9 @@ def index():
         email = data.get('email')
         phone = data.get('phone')
 
-        collection.insert_one({"name": name, "email": email, "phone": phone})
-        return jsonify({"message": "Data saved successfully!"}), 200
+        # Ghi dữ liệu vào Google Sheet
+        sheet.append_row([name, email, phone])
+        return jsonify({"message": "Data saved to Google Sheet!"}), 200
 
     return render_template('index.html')
 
